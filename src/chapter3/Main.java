371bd4dd0d587aa4476baad3d619b99a1b604785
xaehub <novxae@gmail.com>
2025-03-12 20:03:08 +0900

    chore: 자바 스트림 관련 예제 코드 추가 v2

--- src/chapter3/Main.java
+++ src/chapter3/Main.java
@@ -1,17 +1,14 @@
 package chapter3;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("김태정", "이준호", "박수진");
+        String[] arr = {"김태정", "신성록", "박수진"};
 
-        // 스트림 생성
-        Stream<String> stream = names.stream();
-
-        // 스트림 사용 (출력)
+        // 배열을 스트림으로 변환
+        Stream<String> stream = Arrays.stream(arr);
         stream.forEach(System.out::println);
     }
 }
